9089c04675f2353b29bd48db21340fe2fddccc69
seanhillweb <seanhillweb@gmail.com>
2023-08-10 14:16:36 -0500

    feat: add attribution component to main layout

---
+++ src/components/attribution.js
@@ -0,0 +1,17 @@
+/**
+ * Summary. Attribution for Frontend Mentor and Sean Hill.
+ */
+
+export default function Attribution() {
+  return (
+    <footer className="bg-brand-blue text-grey-blue text-sm text-center px-4 py-3" role="contentinfo" aria-label="Site">
+      Challenge by{" "}
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-grey-blue-300 underline hover:no-underline focus:no-underline">
+        Frontend Mentor
+      </a>
+      . Coded by <a href="https://www.seanhillweb.com" className="text-grey-blue-300 underline hover:no-underline focus:no-underline">Sean Hill</a>.
+    </footer>
+  )
+}
+
+
