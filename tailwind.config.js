63e037224779e7db50a00fa83c8e061103519d37
seanhillweb <seanhillweb@gmail.com>
2023-08-11 19:43:24 -0500

    feat: format page structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -20,6 +20,11 @@ module.exports = {
           }
         },
       },
+      gridTemplateRows: {
+        '7': 'repeat(7, minmax(0, 1fr))',
+        '8': 'repeat(8, minmax(0, 1fr))',
+        '9': 'repeat(9, minmax(0, 1fr))',
+      }
     },
     container: {
       center: true,
