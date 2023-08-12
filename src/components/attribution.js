116ff311203053dedb398936c4154a0ba4fdfeb6
seanhillweb <seanhillweb@gmail.com>
2023-08-11 19:43:05 -0500

    fix: syntax

--- src/components/attribution.js
+++ src/components/attribution.js
@@ -4,12 +4,12 @@
 
 export default function Attribution() {
   return (
-    <footer className="bg-brand-blue text-grey-blue text-sm text-center px-4 py-3" role="contentinfo" aria-label="Site">
+    <footer className="bg-brand-violet-100 text-brand-violet-200 text-sm text-center px-4 py-3" role="contentinfo" aria-label="Site">
       Challenge by{" "}
-      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-grey-blue-300 underline hover:no-underline focus:no-underline">
+      <a href="https://www.frontendmentor.io?ref=challenge" target="_blank" className="text-brand-violet-300 underline hover:no-underline focus:no-underline">
         Frontend Mentor
       </a>
-      . Coded by <a href="https://www.seanhillweb.com" className="text-grey-blue-300 underline hover:no-underline focus:no-underline">Sean Hill</a>.
+      . Coded by <a href="https://www.seanhillweb.com" className="text-brand-violet-300 underline hover:no-underline focus:no-underline">Sean Hill</a>.
     </footer>
   )
 }
