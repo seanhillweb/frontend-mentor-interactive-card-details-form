7b2a43e78f32a5d761c77c2cc23076fca3f9c401
seanhillweb <seanhillweb@gmail.com>
2023-08-13 11:02:26 -0500

    fix: cvc number position

--- src/components/credit-card-back.js
+++ src/components/credit-card-back.js
@@ -5,7 +5,7 @@ export default function CreditCardBack({ cardCVCNumber }) {
 
   return (
     <div className="block relative w-full max-w-[447px] h-full max-h-[245px] mx-auto">
-      <div className="absolute bottom-[45%] sm:bottom-[46%] md:bottom-[46%] right-14">
+      <div className="absolute bottom-[46%] sm:bottom-[47%] md:bottom-[46%] lg:bottom-[47%] right-[13%]">
         <p className="text-sm text-white">
           {cardCVCNumber ? cardCVCNumber : `000`}
         </p>
